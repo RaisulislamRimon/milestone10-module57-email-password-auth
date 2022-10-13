1066f7bb8c94c0ea1d5858c147d162c8de0884b2
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2022-10-14 00:12:06 +0600

    update

--- src/components/RegisterReactBootstrap/RegisterReactBootstrap.js
+++ src/components/RegisterReactBootstrap/RegisterReactBootstrap.js
@@ -2,21 +2,33 @@ import React from "react";
 import { Button, Form } from "react-bootstrap";
 
 const RegisterReactBootstrap = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const password = e.target.password.value;
+    // console.log(e.target.name, e.target.name.value);
+    console.log(name, password);
+  };
   return (
     <div className="container w-50">
-      <Form>
+      <h3 className="text-primary text-center">Please Register</h3>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="email" name="name" placeholder="Enter email" />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Password"
+          />
         </Form.Group>
 
         <Button variant="primary" type="submit">
-          Submit
+          Register
         </Button>
       </Form>
     </div>
